41d2f8d7103ee67aa8dca172d23810658cdb61f4
mikedelong <mikedelong@outlook.com>
2018-06-13 16:09:28 -0400

    refactor: rename variables to line up with their locations

--- code/read.py
+++ code/read.py
@@ -43,41 +43,40 @@ if __name__ == '__main__':
     logger.debug(all_data.columns.values)
     logger.debug(all_data.shape)
     c0 = columns_of_interest[0]
-    c1 = columns_of_interest[2]
-    c2 = columns_of_interest[3]
-    c3 = columns_of_interest[4]
-    c4 = columns_of_interest[5]
-    c5 = columns_of_interest[1]
+    c1 = columns_of_interest[1]
+    c2 = columns_of_interest[2]
+    c3 = columns_of_interest[3]
+    c4 = columns_of_interest[4]
+    c5 = columns_of_interest[5]
 
-    years = sorted(all_data[c4].unique())
+    years = sorted(all_data[c5].unique())
     logger.debug('years: %s' % str(years))
     colormap = cm.viridis
     color_count = 3
     revived = list()
     for year in years:
-        data = all_data[all_data[c4] == year]
+        data = all_data[all_data[c5] == year]
         logger.debug('year data shape: %s' % str(data.shape))
-        uniques = sorted(data[c2].unique())
+        uniques = sorted(data[c3].unique())
         logger.debug('unique blocks: %s' % uniques)
-        revived_items = data[data[c5] > 1][c0].values
+        revived_items = data[data[c1] > 1][c0].values
         revived.extend(revived_items)
 
         color_list = [colors.rgb2hex(colormap(item)) for item in np.linspace(0, 0.9, color_count)]
         axes = plt.gca()
         for index, item in enumerate(uniques):
-            color_index = 0 if item < 40 else 1
-            color = color_list[color_index]
-            data_to_plot = data[data[c2] == item]
-            life_remaining = data_to_plot[(data_to_plot[c3] > 0) & (~data_to_plot[c0].isin(revived))]
+            color = color_list[0 if item < 40 else 1]
+            data_to_plot = data[data[c3] == item]
+            life_remaining = data_to_plot[(data_to_plot[c4] > 0) & (~data_to_plot[c0].isin(revived))]
             logger.debug('life remaining shape: %s' % str(life_remaining.shape))
-            life_remaining.plot(kind='scatter', x=c1, y=c3, ax=axes, c=color)
-            no_life_remaining = data_to_plot[(data_to_plot[c3] == 0) & (~data_to_plot[c0].isin(revived))]
+            life_remaining.plot(kind='scatter', x=c2, y=c4, ax=axes, c=color)
+            no_life_remaining = data_to_plot[(data_to_plot[c4] == 0) & (~data_to_plot[c0].isin(revived))]
             logger.debug('no life remaining shape: %s' % str(no_life_remaining.shape))
             if len(no_life_remaining) > 0:
-                no_life_remaining.plot(kind='scatter', x=c1, y=c3, ax=axes, c='r')
+                no_life_remaining.plot(kind='scatter', x=c2, y=c4, ax=axes, c='r')
             new_life = data_to_plot[data_to_plot[c0].isin(revived)]
             if len(new_life) > 0:
-                new_life.plot(kind='scatter', x=c1, y=c3, ax=axes, c=color_list[2])
+                new_life.plot(kind='scatter', x=c2, y=c4, ax=axes, c=color_list[2])
 
         output_filename = '../output/year{:02d}.png'.format(year)
         logger.debug('writing scatter plot to %s' % output_filename)
