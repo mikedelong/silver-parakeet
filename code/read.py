2d8e1989dbbd47bebddcbaa4d24e03b905b6685a
mikedelong <mikedelong@outlook.com>
2018-06-14 11:00:52 -0400

    feature: rename the columns in the data frame so the scatter plot has the right axis labels.

--- code/read.py
+++ code/read.py
@@ -38,16 +38,20 @@ if __name__ == '__main__':
     else:
         logger.warning('required setting %s not supplied. Quitting.' % columns_of_interest_key)
         quit()
+    c0, c1, c2, c3, c4, c5 = columns_of_interest
+
+    rename_columns = None
+    rename_columns_key = 'rename_columns'
+    if rename_columns_key in settings.keys():
+        rename_columns = settings[rename_columns_key]
+    else:
+        logger.warning('required setting %s not supplied. Quitting.' % rename_columns_key)
+        quit()
+    r0, r1 = rename_columns
 
     all_data = pd.read_csv(input_file, usecols=columns_of_interest)
     logger.debug(all_data.columns.values)
     logger.debug(all_data.shape)
-    c0 = columns_of_interest[0]
-    c1 = columns_of_interest[1]
-    c2 = columns_of_interest[2]
-    c3 = columns_of_interest[3]
-    c4 = columns_of_interest[4]
-    c5 = columns_of_interest[5]
 
     years = sorted(all_data[c5].unique())
     logger.debug('years: %s' % str(years))
@@ -64,19 +68,21 @@ if __name__ == '__main__':
 
         color_list = [colors.rgb2hex(colormap(item)) for item in np.linspace(0, 0.9, color_count)]
         axes = plt.gca()
+        mapper = {c2: r1, c4: r0}
         for index, item in enumerate(uniques):
             color = color_list[0 if item < 40 else 1]
             data_to_plot = data[data[c3] == item]
-            life_remaining = data_to_plot[(data_to_plot[c4] > 0) & (~data_to_plot[c0].isin(revived))]
+            data_to_plot.rename(mapper, axis='columns', inplace=True)
+            life_remaining = data_to_plot[(data_to_plot[r0] > 0) & (~data_to_plot[c0].isin(revived))]
             logger.debug('life remaining shape: %s' % str(life_remaining.shape))
-            life_remaining.plot(kind='scatter', x=c2, y=c4, ax=axes, c=color)
-            no_life_remaining = data_to_plot[(data_to_plot[c4] == 0) & (~data_to_plot[c0].isin(revived))]
+            life_remaining.plot(kind='scatter', x=r1, y=r0, ax=axes, c=color)
+            no_life_remaining = data_to_plot[(data_to_plot[r0] == 0) & (~data_to_plot[c0].isin(revived))]
             logger.debug('no life remaining shape: %s' % str(no_life_remaining.shape))
             if len(no_life_remaining) > 0:
-                no_life_remaining.plot(kind='scatter', x=c2, y=c4, ax=axes, c='r')
+                no_life_remaining.plot(kind='scatter', x=r1, y=r0, ax=axes, c='r')
             new_life = data_to_plot[data_to_plot[c0].isin(revived)]
             if len(new_life) > 0:
-                new_life.plot(kind='scatter', x=c2, y=c4, ax=axes, c=color_list[2])
+                new_life.plot(kind='scatter', x=r1, y=r0, ax=axes, c=color_list[2])
 
         output_filename = '../output/year{:02d}.png'.format(year)
         logger.debug('writing scatter plot to %s' % output_filename)
